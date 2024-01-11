483e54593f246be5b9ed5dfdc9286d2dd306d310
Keneth Miler <miler@users.itp.uni-frankfurt.de>
2024-01-11 21:19:30 +0100

    This was a test, if mainprint.py gets changed

--- rns_driver/model/main_print.py
+++ rns_driver/model/main_print.py
@@ -3,6 +3,7 @@ from mpi4py import MPI
 from scipy import stats
 from filter import *
 
+#test if everything is being added
 #Plotting
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
